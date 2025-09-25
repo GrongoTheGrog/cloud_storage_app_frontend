5779983968f54ca704b2f1326da633b7079971fa
Bruno Hugo <brunohugo2007@gmail.com>
2025-09-25 19:10:46 -0300

    Fixed oauth2 redirect bug.

--- app/oauth2/page.tsx
+++ app/oauth2/page.tsx
@@ -47,25 +47,41 @@ const page = () => {
         }
 
         router.push("/");
+        
+        try{
+            fetchCookie(code);
 
-        fetchCookie(code);
+            const auth: AuthType = {
+                email,
+                id: Number(id),
+                picture,
+                username
+            }
 
-        const auth: AuthType = {
-            email,
-            id: Number(id),
-            picture,
-            username
+            authContext.setAuth(auth);
+
+            toast.setToast({
+                message: "Logged in successfully.",
+                status: 200,
+                type: "SUCCESS"
+            });
+        }catch(error){
+            if(axios.isAxiosError(error)){
+                toast.setToast({
+                    message: error.response?.data.message,
+                    status: error.response?.data.status,
+                    type: "ERROR"
+                })
+            }else{
+                toast.setToast({
+                    message: "Unknown error",
+                    status: null,
+                    type: "ERROR"
+                })
+            }
         }
-
-        authContext.setAuth(auth);
-
-        toast.setToast({
-            message: "Logged in successfully.",
-            status: 200,
-            type: "SUCCESS"
-        });
+        
     }, [])
-
 
     return (
         <div className='font-bold text-[34px] text-center mt-[200px]'>
